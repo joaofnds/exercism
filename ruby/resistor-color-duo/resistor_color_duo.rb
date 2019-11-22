f1fcf1a8dc8d81801248c4404c64d93093d667b6
João Fernandes <joaofnds@joaofnds.com>
2019-11-22 13:18:21 -0300

    [ruby] complete resistor-color-duo

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,28 @@
+# Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
+# `resistor_color_duo_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/resistor-color-duo` directory.
+
+class ResistorColorDuo
+  COLORS_VALUES = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }.freeze
+
+  def self.value(colors)
+    color_value(colors[0].to_sym, 10) + color_value(colors[1].to_sym)
+  end
+
+  def self.color_value(color, weight = 1)
+    COLORS_VALUES[color] * weight
+  end
+end
