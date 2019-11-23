bc5b5dc850237c60bf869ed3dad58f10a4d363b5
João Fernandes <joaofnds@joaofnds.com>
2019-11-23 13:53:24 -0300

    [ruby] update rna transcription to use `String#tr`

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,20 +1,5 @@
 class Complement
   def self.of_dna(dna)
-    dna.split('').map { |nucleotide| rna_complement(nucleotide) }.join('')
-  end
-
-  def self.rna_complement(dna_nucleotide)
-    case dna_nucleotide
-    when "C"
-      "G"
-    when "G"
-      "C"
-    when "T"
-      "A"
-    when "A"
-      "U"
-    else
-      ""
-    end
+    dna.tr("GCTA","CGAU")
   end
 end
