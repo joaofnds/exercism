3cf5ba3236b838213789901476a2fce6ef1dfabb
João Fernandes <joaofnds@joaofnds.com>
2019-12-31 00:01:55 -0300

    [ruby][word-count] fixes after mentor feedback
    - rename `string` to `phrase`
      this expresses better my intent
    - do the word that `tally` does, by hand
      this is what the exercise wants me to do after all
    - update regexp to use `[[:word:]]`
      it has better support for UTF8 and better expresses the regexp intent

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,17 +1,19 @@
 class Phrase
-  def initialize(string)
-    @string = string
+  def initialize(phrase)
+    @phrase = phrase
   end
 
   def word_count
-    words.map(&:downcase).tally
+    words
+      .map(&:downcase)
+      .each_with_object(Hash.new(0)) { |word, memo| memo[word] += 1 }
   end
 
   private
 
-  attr_reader :string
+  attr_reader :phrase
 
   def words
-    string.scan(/\w+(?:'\w)*/)
+    phrase.scan(/\b[[:word:]']+\b/)
   end
 end
