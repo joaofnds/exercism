fb3e2ec6987df12e4c27da0a80ac379b04e17656
João Fernandes <joaofnds@joaofnds.com>
2020-01-02 15:12:35 -0300

    [ruby][armstrong-count] use `#sum` instead of `#reduce`

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -4,7 +4,7 @@ class ArmstrongNumbers
   end
 
   def self.armstrong_sum(number)
-    power = number.digits.length
-    number.digits.reduce(0) { |sum, n| sum + n**power }
+    digits = number.digits
+    digits.sum { |digit| digit**digits.count }
   end
 end
