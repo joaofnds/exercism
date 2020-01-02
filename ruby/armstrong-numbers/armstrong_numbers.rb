99ff0b9ffc4b981188cb0f150399dc3c62150118
João Fernandes <joaofnds@joaofnds.com>
2020-01-02 15:10:01 -0300

    [ruby] complete armstrong-numbers

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -1,8 +1,10 @@
-=begin
-Write your code for the 'Armstrong Numbers' exercise in this file. Make the tests in
-`armstrong_numbers_test.rb` pass.
+class ArmstrongNumbers
+  def self.include?(number)
+    armstrong_sum(number) == number
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/armstrong-numbers` directory.
-=end
-
+  def self.armstrong_sum(number)
+    power = number.digits.length
+    number.digits.reduce(0) { |sum, n| sum + n**power }
+  end
+end
