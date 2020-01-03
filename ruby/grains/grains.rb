538d1eb8bbb3a97bbff7fcefa24a09b58b59c041
João Fernandes <joaofnds@joaofnds.com>
2020-01-03 12:23:08 -0300

    [ruby] complete grains

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,8 +1,30 @@
-=begin
-Write your code for the 'Grains' exercise in this file. Make the tests in
-`grains_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/grains` directory.
-=end
+class Grains
+  MIN_SQUARE = 1
+  MAX_SQUARE = 64
 
+  private_constant :MIN_SQUARE, :MAX_SQUARE
+
+  def self.square(square_number)
+    raise BoardSquareError unless valid_board_square?(square_number)
+
+    1 << (square_number - 1)
+  end
+
+  def self.total
+    # as each square doubles, the total amount of grains on the board is
+    # always one less than the grains that would be in the next square
+    square(MAX_SQUARE) * 2 - 1
+  end
+
+  def self.valid_board_square?(square_number)
+    MIN_SQUARE <= square_number && square_number <= MAX_SQUARE
+  end
+end
+
+class BoardSquareError < ArgumentError
+  def message
+    'not a valid board square number'
+  end
+end
