3389bd8c9d18a7ff67b0fa0e82152d31c50dd342
João Fernandes <joaofnds@joaofnds.com>
2020-01-04 07:49:43 -0300

    [ruby] complete nucleotide-count

---
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -0,0 +1,30 @@
+class Nucleotide
+  NUCLEOTIDES = %w[A C T G]
+
+  def self.valid_dna_strand?(dna_strand)
+    dna_strand =~ /^[#{NUCLEOTIDES}]*$/
+  end
+
+  def self.from_dna(dna_strand)
+    raise ArgumentError unless valid_dna_strand? dna_strand
+
+    new(dna_strand)
+  end
+
+  private
+
+  attr_reader :strand
+
+  def initialize(dna_strand)
+    @dna_strand = dna_strand
+    @histogram = NUCLEOTIDES.map { |n| [n, dna_strand.count(n)] }.to_h
+  end
+
+  public
+
+  attr_reader :histogram
+
+  def count(nucleotide)
+    histogram[nucleotide]
+  end
+end
