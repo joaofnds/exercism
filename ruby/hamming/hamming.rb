e40312dccb7d666b1ae07c0620b11cb4d39520a1
João Fernandes <joaofnds@joaofnds.com>
2020-01-07 14:28:51 -0300

    [ruby][hamming] extract dna class. Avoid self methods

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,40 +1,60 @@
 # frozen_string_literal: true
 
+class DNA
+  NUCLEOTIDES = %w[A C T G].freeze
+
+  private_constant :NUCLEOTIDES
+
+  def initialize(strand)
+    @strand = strand
+  end
+
+  def valid_strand?
+    strand =~ /^[#{NUCLEOTIDES}]*$/
+  end
+
+  private
+
+  attr_reader :strand
+end
+
 class Hamming
-  DNA_NUCLEOTIDES = %w[A C T G].freeze
+  class DifferentLengthStrandsError < ArgumentError; end
+  class InvalidDNANucleotideError < ArgumentError; end
 
   def self.compute(strand_a, strand_b)
-    assert_comparable_strands(strand_a, strand_b)
+    new(strand_a, strand_b).distance
+  end
+
+  def initialize(strand_a, strand_b)
+    @strand_a = strand_a
+    @strand_b = strand_b
+  end
+
+  def distance
+    assert_comparable_strands
 
     pairs = strand_a.chars.zip(strand_b.chars)
     pairs.count { |a, b| a != b }
   end
 
-  def self.assert_comparable_strands(*strands)
-    unless strands.map(&:length).uniq.length == 1
-      raise DifferentLengthStrandsError
+  private
+
+  attr_reader :strand_a, :strand_b
+
+  def assert_comparable_strands
+    unless strand_a.length == strand_b.length
+      raise DifferentLengthStrandsError, 'different length strands'
     end
 
-    strands.each do |strand|
-      raise InvalidDNANucleotideError unless dna_strand?(strand)
+    unless DNA.new(strand_a).valid_strand?
+      raise InvalidDNANucleotideError, 'strand A is not a valid DNA strand'
+    end
+
+    unless DNA.new(strand_b).valid_strand?
+      raise InvalidDNANucleotideError, 'strand B is not a valid DNA strand'
     end
 
     true
-  end
-
-  def self.dna_strand?(strand)
-    strand =~ /^[#{DNA_NUCLEOTIDES}]*$/
-  end
-end
-
-class DifferentLengthStrandsError < ArgumentError
-  def message
-    'cannot compare different length strands'
-  end
-end
-
-class InvalidDNANucleotideError < ArgumentError
-  def message
-    'DNA nucleotides must be on of A, C, T or G'
   end
 end
