7c6faea30e1319c55e926a6d1b58d0cbe616d556
João Fernandes <joaofnds@joaofnds.com>
2020-01-09 00:40:42 -0300

    [ruby][hamming] start from scratch

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,29 +1,8 @@
-require_relative 'strand'
-
 class Hamming
-  class SequenceLengthError < ArgumentError; end
+  def self.compute(strand1, strand2)
+    raise ArgumentError unless strand1.length == strand2.length
+    raise ArgumentError if (strand1 + strand2) =~ /^[ACTG]$/
 
-  def self.compute(sequence1, sequence2)
-    strand1 = Strand.new(sequence1.chars)
-    strand2 = Strand.new(sequence2.chars)
-
-    new(strand1, strand2).distance
-  end
-
-  def distance
-    sequence1.zip(sequence2).count { |m1, m2| m1 != m2 }
-  end
-
-  private
-
-  attr_reader :sequence1, :sequence2
-
-  def initialize(sequence1, sequence2)
-    unless sequence1.length == sequence2.length
-      raise SequenceLengthError, 'sequences must have the same length'
-    end
-
-    @sequence1 = sequence1
-    @sequence2 = sequence2
+    strand1.chars.zip(strand2.chars).count { |n1, n2| n1 != n2 }
   end
 end
