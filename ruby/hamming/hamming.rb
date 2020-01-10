d3808ceb6bfd58bfcc988a7dda9abeeeb9175810
João Fernandes <joaofnds@joaofnds.com>
2020-01-09 23:29:56 -0300

    [ruby][hamming] instantiate class. create error
    also remove nucleotide validation

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,30 @@
 class Hamming
+  class StrandLengthsError < ArgumentError
+    def initialize(message = 'strands must have equal lengths')
+      super
+    end
+  end
+
   def self.compute(strand1, strand2)
-    raise ArgumentError unless strand1.length == strand2.length
-    raise ArgumentError if (strand1 + strand2) =~ /^[ACTG]$/
+    new(strand1, strand2).distance
+  end
+
+  def distance
+    raise StrandLengthsError unless equal_lengths?
 
     strand1.chars.zip(strand2.chars).count { |n1, n2| n1 != n2 }
+  end
+
+  private
+
+  attr_reader :strand1, :strand2
+
+  def initialize(strand1, strand2)
+    @strand1 = strand1
+    @strand2 = strand2
+  end
+
+  def equal_lengths?
+    strand1.length == strand2.length
   end
 end
