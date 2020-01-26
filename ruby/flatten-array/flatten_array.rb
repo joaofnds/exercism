96a523256e2c65bdd4306658eb071440032715d3
João Fernandes <joaofnds@joaofnds.com>
2020-01-26 00:32:38 -0300

    [ruby][flatten-array] add note and rename memo variable

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,13 +1,18 @@
 class FlattenArray
+  # we can achieve the desired result with `#flatten` and `#compact`
+  # def self.flatten(array)
+  #   array.flatten.compact
+  # end
+
   def self.flatten(array)
-    array.each_with_object([]) do |element, array|
+    array.each_with_object([]) do |element, memo|
       case element
       when nil
         next
       when Array
-        array.concat flatten(element)
+        memo.concat flatten(element)
       else
-        array.push element
+        memo.push element
       end
     end
   end
