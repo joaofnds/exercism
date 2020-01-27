76b917a9201720dbcbea10a606721ed0c9df6597
João Fernandes <joaofnds@joaofnds.com>
2020-01-27 19:47:32 -0300

    [ruby] complete resistor-color-trio

--- ruby/resistor-color-trio/resistor_color_trio.rb
+++ ruby/resistor-color-trio/resistor_color_trio.rb
@@ -1,8 +1,43 @@
-=begin
-Write your code for the 'Resistor Color Trio' exercise in this file. Make the tests in
-`resistor_color_trio_test.rb` pass.
+class ResistorColorTrio
+  class ResistorColorError < ArgumentError; end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-trio` directory.
-=end
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
 
+  def label
+    if ohms > 1000
+      "Resistor value: #{ohms / 1000} kiloohms"
+    else
+      "Resistor value: #{ohms} ohms"
+    end
+  end
+
+  private
+
+  attr_reader :colors, :ohms
+
+  def initialize(colors)
+    @colors = colors
+    assert_resistor_colors
+
+    @ohms = resistance
+  end
+
+  def color_code(color)
+    COLORS.index color
+  end
+
+  def resistance
+    power = 10**color_code(colors.last)
+
+    first_color_value = color_code(colors[0]) * 10
+    second_color_value = color_code(colors[1])
+
+    (first_color_value + second_color_value) * power
+  end
+
+  def assert_resistor_colors
+    all_colors_have_codes = colors.all? { color_code _1 }
+
+    raise ResistorColorError unless all_colors_have_codes
+  end
+end
