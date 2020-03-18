cad96830a092a16a7fb6b8dccc98d12dacc389d7
João Fernandes <joaofnds@joaofnds.com>
2020-03-17 22:50:27 -0300

    [clojure][nucleotide-count] use frequencies to count nucleotides
    Also add :pre to count-of-nucleotide-in-strand

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,9 +1,9 @@
 (ns nucleotide-count)
 
-(defn count-of-nucleotide-in-strand [nucleotide strand]
-  (get (nucleotide-counts strand) nucleotide))
-
 (defn nucleotide-counts [strand]
-  (reduce #(merge-with + %1 {%2 1})
-          {\A 0 \T 0 \C 0 \G 0}
-          strand))
+   (merge {\A 0 \T 0 \C 0 \G 0}
+         (frequencies strand)))
+
+(defn count-of-nucleotide-in-strand [nucleotide strand]
+  {:pre [(contains? #{\A \T \C \G} nucleotide)]}
+  ((nucleotide-counts strand) nucleotide))
