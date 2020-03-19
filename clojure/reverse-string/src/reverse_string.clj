b3b338d52ae0796096c3c3eeeb7a08a37aa2ced9
João Fernandes <joaofnds@joaofnds.com>
2020-03-19 19:43:16 -0300

    [clojure][reverse-string] comment other ways to solve it

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -3,3 +3,7 @@
 (defn reverse-string [s]
   "Reverse a string."
   (apply str (reverse s)))
+
+;; A couple other ways you could go about solving this problem
+; (apply str (into () s))
+; (reduce #(str %2 %1) "" s)
