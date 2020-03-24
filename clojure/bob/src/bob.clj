cf11acdb2f6338f1b5186b2eddda66dcbe06834b
João Fernandes <joaofnds@joaofnds.com>
2020-03-24 15:29:44 -0300

    [clojure] complete bob

---
+++ clojure/bob/src/bob.clj
@@ -0,0 +1,27 @@
+(ns bob
+  (:require [clojure.string :as str]))
+
+(def silence? str/blank?)
+
+(defn letter?
+  [s]
+  (re-find #"[a-zA-Z]" s))
+
+(defn shout?
+  [s]
+  (and (letter? s)
+       (= (str/upper-case s) s)))
+
+(defn question?
+  [s]
+  (str/ends-with? s "?"))
+
+(defn response-for
+  [s]
+  (let [s (str/trim s)]
+    (cond
+      (silence? s) "Fine. Be that way!"
+      (and (shout? s) (question? s)) "Calm down, I know what I'm doing!"
+      (shout? s) "Whoa, chill out!"
+      (question? s) "Sure."
+      :else "Whatever.")))
