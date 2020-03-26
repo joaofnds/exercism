93623dcd7c2657d23b34d2e83b8910c9070ff8d8
João Fernandes <joaofnds@joaofnds.com>
2020-03-26 08:05:41 -0300

    [clojure][hamming] do everything inline

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -1,9 +1,5 @@
 (ns hamming)
 
-(defn dna-diferences
-  [strand1 strand2]
-  (filter #(apply not= %) (map vector strand1 strand2)))
-
 (defn distance [strand1 strand2]
   (when (= (count strand1) (count strand2))
-    (count (dna-diferences strand1 strand2))))
+    (count (filter false? (map = strand1 strand2)))))
