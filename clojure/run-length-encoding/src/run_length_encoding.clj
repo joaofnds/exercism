59ff66c961e20c50aedb5a0ba67926df0d682443
João Fernandes <joaofnds@joaofnds.com>
2020-04-12 04:05:24 -0300

    [exercism][run-length-encoding] remove state

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,40 +1,27 @@
 (ns run-length-encoding)
 
-(defn decode-letter
+(defn- decode
   [n letter]
-  (apply str (repeat n letter)))
+  (let [n (Integer/parseInt (or n "1"))]
+    (repeat n letter)))
 
-(defn encode-letter
-  [initial-out n letter]
-  (if (> n 1)
-    (str initial-out n letter)
-    (str initial-out letter)))
+(defn- encode
+  [[char :as letters]]
+  (let [length (count letters)]
+    (if (= 1 length)
+      (str char)
+      (str length char))))
 
 (defn run-length-encode
-  "encodes a string with run-length-encoding"
   [plain-text]
-  (let [last-char (atom nil)
-        count (atom 0)
-        out (atom "")]
-    (doseq [char plain-text]
-      (if (= char @last-char)
-        (swap! count inc)
-        (do
-          (swap! out #(encode-letter % @count @last-char))
-          (reset! last-char char)
-          (reset! count 1))))
-    (swap! out #(encode-letter % @count @last-char))
-    @out))
+  (->> plain-text
+       (partition-by identity)
+       (map encode)
+       (apply str)))
 
 (defn run-length-decode
-  "decodes a run-length-encoded string"
   [cipher-text]
-  (let [out (atom "")
-        count (atom "")]
-    (doseq [char cipher-text]
-      (if (Character/isDigit char)
-        (swap! count #(str % char))
-        (do
-          (swap! out #(str % (decode-letter (if (= @count "") 1 (Integer/parseInt @count)) char)))
-          (reset! count ""))))
-    @out))
+  (->> cipher-text
+       (re-seq #"(\d+)?(\D)")
+       (mapcat #(apply decode (rest %)))
+       (apply str)))
