bc6ad5c032f915c405801a475ed67f84a6a1a92f
João Fernandes <joaofnds@joaofnds.com>
2020-04-12 18:32:45 -0300

    [clojure][run-length-encode] inline `encode` and `decode`

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,27 +1,21 @@
 (ns run-length-encoding)
 
-(defn- decode
-  [n letter]
-  (let [n (Integer/parseInt (or n "1"))]
-    (repeat n letter)))
-
-(defn- encode
-  [[char :as letters]]
-  (let [length (count letters)]
-    (if (= 1 length)
-      (str char)
-      (str length char))))
+(defn- letter-count
+  [seq]
+  (let [len (count seq)]
+    (if (> len 1) len "")))
 
 (defn run-length-encode
   [plain-text]
   (->> plain-text
        (partition-by identity)
-       (map encode)
+       (mapcat (juxt letter-count first))
        (apply str)))
 
 (defn run-length-decode
   [cipher-text]
   (->> cipher-text
        (re-seq #"(\d+)?(\D)")
-       (mapcat #(apply decode (rest %)))
+       (mapcat (fn [[_ n char]]
+                 (repeat (Integer/parseInt (or n "1")) char)))
        (apply str)))
