6ee12d008f93cc5e7ea6e8289a99e7338808394d
João Fernandes <joaofnds@joaofnds.com>
2020-04-19 07:48:24 -0300

    [clojure][etl] convert to array and use `into` to produe the map

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,16 +1,7 @@
 (ns etl)
 
-(defn transform-score
-  [value words]
-  (reduce
-   #(merge %1 {(.toLowerCase %2) value})
-   {}
-   words))
+(defn transform-score [[score words]]
+  (map #(vector (.toLowerCase %) score) words))
 
-(defn transform
-  [source]
-  (reduce
-   (fn [col [value words]]
-     (merge col (transform-score value words)))
-   {}
-   source))
+(defn transform [source]
+  (into {} (mapcat transform-score source)))
