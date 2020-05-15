0daae7c9c4b87d0aa59adaa607c0602251f78e40
João Fernandes <joaofnds@joaofnds.com>
2020-05-15 10:10:21 -0300

    [clojure][isbn-verifier] add isbn10->isbn13

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,5 +1,4 @@
-(ns isbn-verifier
-  (:require [clojure.string :as str]))
+(ns isbn-verifier)
 
 (defn- isbn-schema? [isbn]
   (re-matches #"(?:\d-?){9}[\dX]" isbn))
@@ -19,7 +18,40 @@
        (reduce +)
        (divisible-by? 11)))
 
+(defn- adapt-isbn10-to-isbn13
+  [isbn10]
+  (->> isbn10
+       drop-last
+       (concat "978")
+       isbn->nums))
+
+(defn- isbn13-sum
+  [isbn13-nums]
+  (reduce + (map * isbn13-nums (flatten (repeat [1 3])))))
+
+(defn- isbn13-checksum-digit
+  [sum]
+  (let [remainder (rem sum 10)]
+    (if (zero? remainder)
+      0
+      (- 10 remainder))))
+
+(defn- isbn13-with-checksum-digit
+  [isbn13-without-checksum-digit]
+  (->> isbn13-without-checksum-digit
+       isbn13-sum
+       isbn13-checksum-digit
+       (conj (into [] isbn13-without-checksum-digit))
+       (apply str)))
+
 (defn isbn? [isbn]
   (if (isbn-schema? isbn)
     (checksum? (isbn->nums isbn))
     false))
+
+(defn isbn10->isbn13
+  [isbn10]
+  (when (isbn? isbn10)
+    (-> isbn10
+        adapt-isbn10-to-isbn13
+        isbn13-with-checksum-digit)))
