db4277661f3c1cceae9ac401112e3e42d022f375
João Fernandes <joaofnds@joaofnds.com>
2020-06-09 22:22:49 -0300

    [clojure][anagram] use clojure.string/lower-case and frequencies

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,11 +1,12 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :refer [lower-case]]))
 
 (defn- anagram? [word candidate]
-  (let [lower-word (.toLowerCase word)
-        lower-candidate (.toLowerCase candidate)]
+  (let [lower-word (lower-case word)
+        lower-candidate (lower-case candidate)]
     (and
      (not= lower-word lower-candidate)
-     (= (sort lower-word) (sort lower-candidate)))))
+     (= (frequencies lower-word) (sort lower-candidate)))))
 
 (defn anagrams-for [word prospect-list]
   (filter (partial anagram? word) prospect-list))
