c642a6dd3a9920c07d6e5ed3020af044dd288ca1
João Fernandes <joaofnds@joaofnds.com>
2020-07-25 18:08:08 -0300

    [clojure] (really) Complete hexadecimal
    forgot to stage solution on last commit

--- clojure/hexadecimal/src/hexadecimal.clj
+++ clojure/hexadecimal/src/hexadecimal.clj
@@ -1,5 +1,20 @@
 (ns hexadecimal)
 
-(defn hex-to-int [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn pow [a b]
+  (reduce * 1 (repeat b a)))
+
+(defn valid-binary-number [binary-number-as-string]
+  (re-matches #"[0-9a-f]+" binary-number-as-string))
+
+(defn hex-digit-to-int [binary-char]
+  ({\0 0 \1 1 \2 2  \3 3  \4 4  \5 5  \6 6  \7 7
+    \8 8 \9 9 \a 10 \b 11 \c 12 \d 13 \e 14 \f 15} binary-char))
+
+(defn hex-to-int [num-str]
+  (if (valid-binary-number num-str)
+    (->> num-str
+         (map hex-digit-to-int)
+         reverse
+         (map-indexed #(* %2 (pow 16 %1)))
+         (apply +))
+    0))
