abdc40f22fb0a438c81ad3c38964ce8800dc0c69
João Fernandes <joaofnds@joaofnds.com>
2020-08-29 14:13:48 -0300

    [clojure][isogram] Use `distinct?` and `remove`

--- clojure/isogram/src/isogram.clj
+++ clojure/isogram/src/isogram.clj
@@ -1,8 +1,7 @@
 (ns isogram
   (:require [clojure.string :refer [lower-case]]))
 
-(defn isogram? [a-string]
-  (->> (lower-case a-string)
-       (re-seq #"\p{L}")
-       frequencies
-       (every? #(= 1 (second %)))))
+(defn isogram? [word]
+  (->> (lower-case word)
+       (remove #{\- \space})
+       (apply distinct?)))
