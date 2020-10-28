d518a3887c49b3fff81745b489d95270c475d108
João Fernandes <joaofnds@joaofnds.com>
2020-10-28 08:40:46 -0300

    [lisp] Complete Binary Search

---
+++ common-lisp/binary-search/binary-search.lisp
@@ -0,0 +1,16 @@
+(in-package #:cl-user)
+(defpackage #:binary-search
+  (:use #:common-lisp)
+  (:export #:binary-find #:value-error))
+
+(in-package #:binary-search)
+
+(defun binary-find (arr el)
+  (loop :for left    := 0                 :then (if (< el element) left        (1+ middle))
+        :for right   := (1- (length arr)) :then (if (< el element) (1- middle) right)
+        :for middle  := (+ left (floor (- right left) 2))
+        :while (not (minusp middle))
+        :for element := (elt arr middle)
+        :when (= el element)
+          :do (return middle)
+        :while (< left right)))
