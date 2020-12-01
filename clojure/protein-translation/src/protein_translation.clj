b264d14d086687cbe94549c272fb43698a85f18c
João Fernandes <joaofnds@joaofnds.com>
2020-12-01 07:33:37 -0300

    [clojure] Complete protein-translation

---
+++ clojure/protein-translation/src/protein_translation.clj
@@ -0,0 +1,30 @@
+(ns protein-translation)
+
+(def codon->protein
+  {"AUG" "Methionine"
+   "UUU" "Phenylalanine"
+   "UUC" "Phenylalanine"
+   "UUA" "Leucine"
+   "UUG" "Leucine"
+   "UCU" "Serine"
+   "UCC" "Serine"
+   "UCA" "Serine"
+   "UCG" "Serine"
+   "UAU" "Tyrosine"
+   "UAC" "Tyrosine"
+   "UGU" "Cysteine"
+   "UGC" "Cysteine"
+   "UGG" "Tryptophan"
+   "UAA" "STOP"
+   "UAG" "STOP"
+   "UGA" "STOP"})
+
+(defn translate-codon [codon]
+  (codon->protein codon))
+
+(defn translate-rna [rna]
+  (->>
+   (partition 3 rna)
+   (map (partial apply str))
+   (map translate-codon)
+   (take-while (partial not= "STOP"))))
