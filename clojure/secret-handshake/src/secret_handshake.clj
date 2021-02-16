160464b1d46ecdfc872985df005c7ca48a00fc72
João Fernandes <joaofnds@joaofnds.com>
2021-02-16 07:42:40 -0300

    [clojure] Complete secret-handshake

---
+++ clojure/secret-handshake/src/secret_handshake.clj
@@ -0,0 +1,9 @@
+(ns secret-handshake)
+
+(defn commands [n]
+  (cond-> '()
+    (bit-test n 3) (conj "jump")
+    (bit-test n 2) (conj "close your eyes")
+    (bit-test n 1) (conj "double blink")
+    (bit-test n 0) (conj "wink")
+    (bit-test n 4) (reverse)))
