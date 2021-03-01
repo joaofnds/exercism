5b05ad57b3b45dc53bbb759256fb89da3f9f836f
João Fernandes <joaofnds@joaofnds.com>
2021-03-01 18:23:28 -0300

    [clojure][largest-series-product] remove let

--- clojure/largest-series-product/src/largest_series_product.clj
+++ clojure/largest-series-product/src/largest_series_product.clj
@@ -3,8 +3,8 @@
 (defn largest-product [span numeric-string]
   {:pre [(<= 0 span (count numeric-string)) (re-matches #"\d*" numeric-string)]}
   (if (zero? span) 1
-      (let [numbers (map #(Character/digit % 10) numeric-string)]
-        (->> numbers
-             (partition span 1)
-             (map #(apply * %))
-             (apply max)))))
+      (->> numeric-string
+           (map #(Character/digit % 10))
+           (partition span 1)
+           (map #(apply * %))
+           (apply max))))
