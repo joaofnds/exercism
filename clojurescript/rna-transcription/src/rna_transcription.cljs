502d8ea1e06843c0cd2ac30258f3c59da8b51815
João Fernandes <joaofnds@joaofnds.com>
2021-03-08 03:52:57 -0300

    [clojurescript] complete rna-transcription

---
+++ clojurescript/rna-transcription/src/rna_transcription.cljs
@@ -0,0 +1,14 @@
+(ns rna-transcription)
+
+(def dna->rna-hash
+  {\C \G
+   \G \C
+   \A \U
+   \T \A})
+
+(defn dna-nucleotide-complement [dna-nucleotide]
+  {:pre [(contains? dna->rna-hash dna-nucleotide)]}
+  (get dna->rna-hash dna-nucleotide))
+
+(defn to-rna [dna]
+  (apply str (map dna-nucleotide-complement dna)))
