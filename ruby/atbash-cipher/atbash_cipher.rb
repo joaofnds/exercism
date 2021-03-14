bc1a8b648e5ee7b72eae1267d6d07c00921bfb27
João Fernandes <joaofnds@joaofnds.com>
2021-03-14 18:57:38 -0300

    [ruby][atbash-cipher] simplify substitutions

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,21 +1,14 @@
-ALPHABET = "abcdefghijklmnopqrstuvwxyz".chars
-PAIRS = ALPHABET.zip(ALPHABET.reverse)
-
 class Atbash
-  def self.encode(string)
-    string
-      .downcase
-      .gsub(/[^a-z0-9]/, "")
-      .chars.map { |c| PAIRS.find { _1[0] == c }&.at(1) || c }
-      .each_slice(5)
-      .map(&:join)
-      .join(" ")
+  REVERSE_ALPHABET = ('a'..'z').to_a.reverse.join
+  def self.encode(str)
+    str.downcase
+       .tr('^a-z0-9', '')
+       .tr('a-z', REVERSE_ALPHABET)
+       .scan(/.{1,5}/)
+       .join(' ')
   end
 
-  def self.decode(string)
-    string
-      .delete(" ")
-      .chars.map { |c| PAIRS.find { _1[1] == c }&.at(0) || c }
-      .join
+  def self.decode(str)
+    str.delete(' ').tr('a-z', REVERSE_ALPHABET)
   end
 end
