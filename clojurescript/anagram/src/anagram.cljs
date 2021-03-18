ff2bce34e7a3bf87c70f1c651e0f499cebefd664
João Fernandes <joaofnds@joaofnds.com>
2021-03-18 20:31:24 -0300

    [clojurescript] Complete anagram

---
+++ clojurescript/anagram/src/anagram.cljs
@@ -0,0 +1,12 @@
+(ns anagram
+  (:require [clojure.string :refer [lower-case]]))
+
+(defn- anagram? [word candidate]
+  (let [lower-word (lower-case word)
+        lower-candidate (lower-case candidate)]
+    (and
+     (not= lower-word lower-candidate)
+     (= (sort lower-word) (sort lower-candidate)))))
+
+(defn anagrams-for [word prospect-list]
+  (filter (partial anagram? word) prospect-list))
