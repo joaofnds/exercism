8a42f311caa3b7e15bfafec4ce54c47907cd8648
João Fernandes <joaofnds@joaofnds.com>
2021-03-19 08:11:59 -0300

    [ruby] Complete dominoes

---
+++ ruby/dominoes/dominoes.rb
@@ -0,0 +1,35 @@
+def delet_first(arr, elem)
+  arr = arr.dup
+  arr.delete_at(arr.index(elem) || arr.length)
+  arr
+end
+
+class Dominoes
+  class << self
+    def chain?(stones)
+      stones.empty? || stones.any? { backtrack delet_first(stones, _1), [_1] }
+    end
+
+    def backtrack(stones, chain)
+      if stones.empty?
+        chain.first.first == chain.last.last
+      else
+        stones.any? do |stone|
+          m = match(chain.last, stone)
+          m && backtrack(delet_first(stones, stone), chain + [m])
+        end
+      end
+    end
+
+    def match(d1, d2)
+      _, b = d1
+      c, d = d2
+      case b
+      when c
+        [c, d]
+      when d
+        [d, c]
+      end
+    end
+  end
+end
