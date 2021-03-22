3ef7e956957c0d7e4df4d475d59ba470b4048c0e
João Fernandes <joaofnds@joaofnds.com>
2021-03-22 09:00:00 -0300

    [clojurescript] complete run-length-encoding

---
+++ clojurescript/run-length-encoding/src/run_length_encoding.cljs
@@ -0,0 +1,21 @@
+(ns run-length-encoding)
+
+(defn- letter-count
+  [seq]
+  (let [len (count seq)]
+    (if (> len 1) len "")))
+
+(defn run-length-encode
+  [plain-text]
+  (->> plain-text
+       (partition-by identity)
+       (mapcat (juxt letter-count first))
+       (apply str)))
+
+(defn run-length-decode
+  [cipher-text]
+  (->> cipher-text
+       (re-seq #"(\d+)?(\D)")
+       (mapcat (fn [[_ n char]]
+                 (repeat (js/parseInt (or n "1")) char)))
+       (apply str)))
