845ff9b1463579f32e052fa08803339a3e0c99f2
João Fernandes <joaofnds@joaofnds.com>
2021-04-03 14:15:44 -0300

    [ruby] complete twelve-days

---
+++ ruby/twelve-days/twelve_days.rb
@@ -0,0 +1,37 @@
+class TwelveDays
+  DAYS = %w[first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth]
+
+  GIFTS = [
+    'a Partridge in a Pear Tree',
+    'two Turtle Doves',
+    'three French Hens',
+    'four Calling Birds',
+    'five Gold Rings',
+    'six Geese-a-Laying',
+    'seven Swans-a-Swimming',
+    'eight Maids-a-Milking',
+    'nine Ladies Dancing',
+    'ten Lords-a-Leaping',
+    'eleven Pipers Piping',
+    'twelve Drummers Drumming'
+  ]
+
+  def self.song
+    lyrics = ''
+    tail = ''
+
+    DAYS.each_with_index do |day, i|
+      tail = if i.zero?
+               "#{GIFTS[0]}."
+             elsif i == 1
+               "#{GIFTS[1]}, and #{tail}"
+             else
+               "#{GIFTS[i]}, #{tail}"
+             end
+
+      lyrics += "On the #{day} day of Christmas my true love gave to me: #{tail}\n\n"
+    end
+
+    lyrics.chomp
+  end
+end
