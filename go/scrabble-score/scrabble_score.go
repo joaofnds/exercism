ceddbb59943b5e54bc63419e0f6abd71d63848f3
João Fernandes <joaofnds@joaofnds.com>
2021-04-22 04:29:10 -0300

    [go] complete scrabble-score

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,25 @@
+package scrabble
+
+import "strings"
+
+var letterValue = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
+}
+
+func Score(s string) int {
+	total := 0
+
+	for _, v := range strings.ToUpper(s) {
+		if p, ok := letterValue[v]; ok {
+			total += p
+		}
+	}
+
+	return total
+}
