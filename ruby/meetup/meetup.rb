74757d1a055946422e50b92d841c75d8ddf1e528
João Fernandes <joaofnds@joaofnds.com>
2021-06-18 07:02:48 -0300

    [ruby] Complete meetup

---
+++ ruby/meetup/meetup.rb
@@ -0,0 +1,28 @@
+require "date"
+
+class Meetup
+  WEEKDAYS = [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
+
+  WEEK_OFFSET = {
+    first:  1,
+    second: 8,
+    third:  15,
+    fourth: 22,
+    teenth: 13,
+    last:   -7,
+  }
+
+  def day(weekday, offset)
+    first = Date.new(year, month, WEEK_OFFSET[offset])
+    first + (WEEKDAYS.index(weekday) - first.wday) % 7
+  end
+
+  private
+
+  attr_reader :month, :year
+
+  def initialize(month, year)
+    @month = month
+    @year = year
+  end
+end
