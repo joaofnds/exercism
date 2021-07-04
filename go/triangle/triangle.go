f81e122f08692642e9b8eb884bea727fab3b251f
João Fernandes <joaofnds@joaofnds.com>
2021-07-04 18:39:25 -0300

    [go] complete triangle

---
+++ go/triangle/triangle.go
@@ -0,0 +1,43 @@
+// Package triangle implements the solution to the triangle exercise on exercism
+package triangle
+
+import "math"
+
+type Kind int
+
+const (
+	NaT Kind = -1 // not a triangle
+	Equ Kind = 3  // equilateral
+	Iso Kind = 2  // isosceles
+	Sca Kind = 0  // scalene
+)
+
+// KindFromSides returns the type of triangle given its sides length
+func KindFromSides(a, b, c float64) Kind {
+	if isValidTriangle(a, b, c) {
+		switch {
+		case a != b && b != c && c != a:
+			return Sca
+		case a == b && b == c && c == a:
+			return Equ
+		case a == b || b == c || c == a:
+			return Iso
+		}
+	}
+
+	return NaT
+}
+
+func isValidTriangle(a, b, c float64) bool {
+	for _, v := range []float64{a, b, c} {
+		if math.IsNaN(v) || math.IsInf(v, 0) || v == 0 {
+			return false
+		}
+	}
+
+	if (a+b) < c || (b+c) < a || (c+a) < b {
+		return false
+	}
+
+	return true
+}
