2222bff165d7661ff58479614c857ab39f023338
João Fernandes <joaofnds@joaofnds.com>
2021-09-02 21:49:16 -0300

    [lisp] complete pizza-pi

---
+++ common-lisp/pizza-pi/pizza-pi.lisp
@@ -0,0 +1,21 @@
+(defpackage :pizza-pi
+  (:use :cl)
+  (:export :dough-calculator :pizzas-per-cube
+           :size-from-sauce :fair-share-p))
+
+(in-package :pizza-pi)
+
+(defconstant *slices-per-pizza* 8)
+
+(defun dough-calculator (pizzas diameter)
+  (round (* pizzas (+ (/ (* 45 pi diameter) 20) 200))))
+
+(defun size-from-sauce (sauce)
+  (sqrt (/ (* 40 sauce) (* 3 pi))))
+
+(defun pizzas-per-cube (cube-size diameter)
+  (floor (/ (* 2 (expt cube-size 3))
+            (* 3 pi (expt diameter 2)))))
+
+(defun fair-share-p (pizzas friends)
+  (zerop (rem (* pizzas *slices-per-pizza*) friends)))
