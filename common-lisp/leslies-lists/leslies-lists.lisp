10ec8a33f01fa62e4c198fbfdcab42f4d80592ed
João Fernandes <joaofnds@joaofnds.com>
2021-09-03 13:56:37 -0300

    [lisp] complete leslies-lists

---
+++ common-lisp/leslies-lists/leslies-lists.lisp
@@ -0,0 +1,41 @@
+(defpackage :leslies-lists
+  (:use :cl)
+  (:export :new-list
+           :list-of-things
+           :add-to-list
+           :first-thing
+           :second-thing
+           :third-thing
+           :twenty-third-thing
+           :remove-first-item
+           :on-the-list-p
+           :list-append
+           :just-how-long
+           :part-of-list
+           :list-reverse))
+
+(in-package :leslies-lists)
+
+(defun new-list () (list))
+
+(defun list-of-things (thing1 thing2 thing3)
+  (list thing1 thing2 thing3))
+
+(defun add-to-list (item list)
+  (cons item list))
+
+(defun first-thing  (list) (car list))
+(defun second-thing (list) (cadr list))
+(defun third-thing  (list) (caddr list))
+
+(defun twenty-third-thing (list)
+  (nth 23 list))
+
+(defun remove-first-item (list)
+  (cdr list))
+
+(defun list-append (list1 list2)
+  (concatenate 'list list1 list2))
+
+(defun just-how-long (list)
+  (length list))
