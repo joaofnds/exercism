feb5807330ef82e2fe6ba536b54830679afe2b47
João Fernandes <joaofnds@joaofnds.com>
2021-09-05 23:24:32 -0300

    [lisp][log-levels] assume log level length

--- common-lisp/log-levels/log-levels.lisp
+++ common-lisp/log-levels/log-levels.lisp
@@ -5,22 +5,19 @@
 (in-package :log-levels)
 
 (defun log-message (log-string)
-  (string-trim " " (subseq log-string (1+ (position #\: log-string)))))
-
-(defun log-level (log-string)
-  (string-downcase (subseq log-string 1 (position #\] log-string))))
+  (subseq log-string 8))
 
 (defun log-severity (log-string)
-  (let ((severity (log-level log-string)))
+  (let ((severity (string-downcase (subseq log-string 1 5))))
     (cond
       ((string= severity "info") :everything-ok)
       ((string= severity "ohno") :run-for-cover)
       ((string= severity "warn") :getting-worried))))
 
 (defun log-format (log-string)
-  (let ((msg (log-message log-string))
-        (level (log-severity log-string)))
-    (case level
-      (:everything-ok (string-downcase msg))
-      (:run-for-cover (string-upcase msg))
-      (:getting-worried (string-capitalize msg)))))
+  (let ((message (log-message log-string))
+        (severity (log-severity log-string)))
+    (case severity
+      (:everything-ok (string-downcase message))
+      (:run-for-cover (string-upcase message))
+      (:getting-worried (string-capitalize message)))))
