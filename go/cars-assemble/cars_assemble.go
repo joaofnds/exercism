40b98a302711823168e577b8e6f0e55f646425ee
João Fernandes <joaofnds@joaofnds.com>
2021-09-27 00:28:33 -0300

    [go] complete cars-assemble

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,33 @@
+package cars
+
+const carsPerHour = 221.0
+
+// CalculateProductionRatePerHour for the assembly line, taking into account
+// its success rate
+func CalculateProductionRatePerHour(speed int) float64 {
+	return float64(speed) * carsPerHour * successRate(speed)
+}
+
+// CalculateProductionRatePerMinute describes how many working items are
+// produced by the assembly line every minute
+func CalculateProductionRatePerMinute(speed int) int {
+	return int(CalculateProductionRatePerHour(speed) / 60)
+}
+
+// successRate is used to calculate the ratio of an item being created without
+// error for a given speed
+func successRate(speed int) float64 {
+	if speed == 0 {
+		return 0.0
+	}
+
+	if speed < 5 {
+		return 1.0
+	}
+
+	if speed < 9 {
+		return 0.9
+	}
+
+	return 0.77
+}
