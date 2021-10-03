231a186dc8845fb4c94981691753aeef7d2f34c5
João Fernandes <joaofnds@joaofnds.com>
2021-10-03 13:41:23 -0300

    [lisp] complete gigasecond-anniversary

---
+++ common-lisp/gigasecond-anniversary/gigasecond-anniversary.lisp
@@ -0,0 +1,11 @@
+(defpackage :gigasecond-anniversary
+  (:use :cl)
+  (:export :from))
+(in-package :gigasecond-anniversary)
+
+(defun from (year month day hour minute second)
+  (multiple-value-bind (s mi h d mo y)
+      (decode-universal-time
+       (+ 1000000000 (encode-universal-time second minute hour day month year 0))
+       0)
+    (list y mo d h mi s)))
