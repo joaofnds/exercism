c4e7b6d7f08a0ede44893d04787cef8ae9c271dd
João Fernandes <joaofnds@joaofnds.com>
2021-11-20 08:44:04 -0300

    [go] complete vehicle_purchase

---
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,29 @@
+package purchase
+
+// NeedsLicense determines whether a license is need to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	return kind == "car" || kind == "truck"
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in dictionary order.
+func ChooseVehicle(opt1, opt2 string) string {
+	var opt string
+	if opt2 < opt1 {
+		opt = opt2
+	} else {
+		opt = opt1
+	}
+
+	return opt + " is clearly the better choice."
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 {
+		return originalPrice * 0.8
+	} else if age < 10 {
+		return originalPrice * 0.7
+	} else {
+		return originalPrice * 0.5
+	}
+}
