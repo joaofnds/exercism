044fa703663acee241daf8b032bff220560b40dc
João Fernandes <joaofnds@joaofnds.com>
2021-12-05 10:53:58 -0300

    [clojurescript][pascals-triangle] reorder function

--- clojurescript/pascals-triangle/src/pascals_triangle.cljs
+++ clojurescript/pascals-triangle/src/pascals_triangle.cljs
@@ -3,8 +3,8 @@
 (defn step [row]
   (flatten [1 (map + row (rest row)) 1]))
 
-(defn row [n]
-  (nth triangle (dec n)))
-
 (def triangle
   (iterate step [1]))
+
+(defn row [n]
+  (nth triangle (dec n)))
