d81345a57cec697dd5124a0eef90a045ad7838e6
João Fernandes <joaofnds@joaofnds.com>
2022-08-28 17:10:19 -0300

    [lisp][largest-series-product] refactor

--- common-lisp/largest-series-product/largest-series-product.lisp
+++ common-lisp/largest-series-product/largest-series-product.lisp
@@ -4,16 +4,9 @@
 
 (in-package :largest-series-product)
 
-(defun partition (seq size)
-  (loop :for i :to (- (length seq) size)
-        :collect (subseq seq i (+ i size))))
-
-(defun largest-product (str span)
-  (let ((digits (map 'list #'digit-char-p str)))
-    (cond
-      ((zerop span) 1)
-      ((minusp span) nil)
-      ((< (length digits) span) nil)
-      ((some #'null digits) nil)
-      (t (loop :for series :in (partition digits span)
-               :maximize (apply '* series))))))
+(defun largest-product (digits span)
+  (when (and (<= 0 span (length digits)) (every #'digit-char-p digits))
+    (loop :for i :upto (- (length digits) span)
+          :for series := (subseq digits i (+ i span))
+          :for product := (apply #'* (map 'list #'digit-char-p series))
+          :maximize product)))
