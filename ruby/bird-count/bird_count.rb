e6fe2f8d739bd3199985d455c16c9775594863f8
João Fernandes <joaofnds@joaofnds.com>
2023-01-15 19:52:05 -0300

    [ruby] complete bird-count

---
+++ ruby/bird-count/bird_count.rb
@@ -0,0 +1,29 @@
+class BirdCount
+  def self.last_week
+    [0, 2, 5, 3, 7, 8, 4]
+  end
+
+  def yesterday
+    birds_per_day[-2]
+  end
+
+  def total
+    birds_per_day.sum
+  end
+
+  def busy_days
+    birds_per_day.count { _1 >= 5 }
+  end
+
+  def day_without_birds?
+    birds_per_day.include?(0)
+  end
+
+  private
+
+  attr_reader :birds_per_day
+
+  def initialize(birds_per_day)
+    @birds_per_day = birds_per_day
+  end
+end
