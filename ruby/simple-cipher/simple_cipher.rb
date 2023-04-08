da36963b163fbae0841b18c2b74bacaa864b256d
João Fernandes <joaofnds@joaofnds.com>
2023-04-07 23:30:53 -0300

    [ruby] complete simple-cipher

---
+++ ruby/simple-cipher/simple_cipher.rb
@@ -0,0 +1,28 @@
+class Cipher
+  ALPHABET = ('a'..'z').to_a
+  attr_reader :key
+
+  def initialize(key = nil)
+    @key = key || 100.times.map { ALPHABET.sample }.join
+    raise ArgumentError, 'invalid chars in key' unless valid?(@key)
+  end
+
+  def encode(text)
+    a = 'a'.ord
+    text.chars.zip(@key.chars).map do |char, key|
+      ALPHABET[(char.ord - a + key.ord - a) % ALPHABET.length]
+    end.join
+  end
+
+  def decode(code)
+    code.chars.zip(@key.chars).map do |char, key|
+      ALPHABET[char.ord - key.ord]
+    end.join
+  end
+
+  private
+
+  def valid?(key)
+    !key.empty? && key !~ /[^a-z]/
+  end
+end
